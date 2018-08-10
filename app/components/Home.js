c161ec3413eb8ad24b46aff026f62c085592cb63
Yotam Manor <yotammanor@gmail.com>
2018-08-10 18:45:32 +0300

    stage 10: add deploy configuration, and fix for prod.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {LocationForm} from "./LocationForm";
-import 'file-loader!../images/pattern.svg'
 
 class Home extends React.Component {
   render() {
+    const backgroundImageUrl = `url('${require('../images/pattern.svg')}')`;
     return (
-      <div className={"main-container"} style={{backgroundImage: "url('../images/pattern.svg')"}}>
+      <div className={"main-container"} style={{backgroundImage: backgroundImageUrl}}>
         <h1 className={"header"}>Enter a City and State</h1>
         <LocationForm style={{flexDirection: "column"}}/>
       </div>
